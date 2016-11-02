4722303fdce3b326fa4f60a7a5a0ee4911c522af
abheshek cr <cabheshek@gmail.com>
2016-11-02 17:00:12 +0530

    second

---
+++ app/src/main/java/com/example/models/moviesapp/MovieModel.java
@@ -0,0 +1,65 @@
+package com.example.models.moviesapp;
+
+/**
+ * Created by welcome on 11/2/2016.
+ */
+
+public class MovieModel {
+
+    private String poster_path;
+    private String overview;
+    private String  release_date;
+    private String original_title;
+
+    public Integer getVote_average() {
+        return vote_average;
+    }
+
+    public void setVote_average(Integer vote_average) {
+        this.vote_average = vote_average;
+    }
+
+    private Integer vote_average;
+
+
+
+    public String getOriginal_title() {
+        return original_title;
+    }
+
+    public void setOriginal_title(String original_title) {
+        this.original_title = original_title;
+    }
+
+    public String getRelease_date() {
+        return release_date;
+    }
+
+    public void setRelease_date(String release_date) {
+        this.release_date = release_date;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public void setOverview(String overview) {
+        this.overview = overview;
+    }
+
+    public String getPoster_path() {
+        return poster_path;
+    }
+
+    public void setPoster_path(String poster_path) {
+        this.poster_path = poster_path;
+    }
+
+
+
+}
+
+
+
+
+
--- app/src/main/java/com/example/welcome/movieapp/MainActivity.java
+++ app/src/main/java/com/example/welcome/movieapp/MainActivity.java
@@ -1,13 +1,143 @@
 package com.example.welcome.movieapp;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.models.moviesapp.MovieModel;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
 
 public class MainActivity extends AppCompatActivity {
+
+    HttpsURLConnection urlConnection;;
+    BufferedReader reader;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn = (Button)findViewById(R.id.button);
+        textView =(TextView)findViewById(R.id.textView2);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                new JsonTask().execute("https://api.themoviedb.org/3/movie/popular?api_key=18e04e15899be92b78b879f04b8b0934");
+            }
+        });
+
+
+
+
+    }
+
+    public class JsonTask extends AsyncTask<String,String,String >{
+        @Override
+        protected String doInBackground(String... params) {
+
+            try {
+                URL url = new URL(params[0]);
+                HttpsURLConnection connection = (HttpsURLConnection)url.openConnection();
+
+                connection.connect();
+
+                InputStream stream = connection.getInputStream();
+
+                reader = new BufferedReader(new InputStreamReader(stream));
+
+                String line ="";
+
+                StringBuffer buffer = new StringBuffer();
+
+                while ((line =reader.readLine())!=null){
+
+                    buffer.append(line);
+
+                }
+               String finalJson =  buffer.toString();
+
+                JSONObject parentJson = new JSONObject(finalJson);
+
+                JSONArray movieArray = parentJson.getJSONArray("results");
+
+                StringBuffer finalBufferedData = new StringBuffer();
+
+                for(int i=0;i<movieArray.length();i++){
+
+                    JSONObject jsonObject = movieArray.getJSONObject(i);
+
+                    MovieModel movieModel = new MovieModel();
+
+                    movieModel.setOriginal_title(jsonObject.getString("original_title"));
+
+                    movieModel.setPoster_path(jsonObject.getString("poster_path"));
+
+                    movieModel.setOverview(jsonObject.getString("overview"));
+
+                    movieModel.setRelease_date(jsonObject.getString("release_date"));
+
+                    movieModel.setVote_average(jsonObject.getInt("vote_average"));
+
+
+
+                }
+
+                return finalBufferedData.toString();
+
+
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            } finally {
+
+
+                if(urlConnection!=null){
+                    urlConnection.disconnect();
+                }
+                try {
+                    if(reader!=null){
+                        reader.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+            textView.setText(result);
+            Log.i("appInfo",result);
+        }
     }
 }
